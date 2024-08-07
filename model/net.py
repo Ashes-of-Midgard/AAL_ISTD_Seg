39e58918a7211fc308bb0bdb4f5e4275a5a1bbde
yyl <20231149@buaa.edu.cn>
2024-08-07 15:09:15 +0800

    Modify Res_CBAM_block implementation and append Res_SP_block class

--- model/net.py
+++ model/net.py
@@ -29,6 +29,7 @@ class Res_block(nn.Module):
         out = self.relu(out)
         return out
 
+
 class SpatialAttention(nn.Module):
     def __init__(self, kernel_size=7):
         super(SpatialAttention, self).__init__()
@@ -42,6 +43,23 @@ class SpatialAttention(nn.Module):
         x = torch.cat([avg_out, max_out], dim=1)
         x = self.conv1(x)
         return self.sigmoid(x)
+
+
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.fc1   = nn.Conv2d(in_planes, in_planes // 8, 1, bias=False)
+        self.relu1 = nn.ReLU()
+        self.fc2   = nn.Conv2d(in_planes // 8, in_planes, 1, bias=False)
+        self.sigmoid = nn.Sigmoid()
+    def forward(self, x):
+        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
+        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out)
+
 
 class Res_CBAM_block(nn.Module):
     def __init__(self, in_channels, out_channels, stride = 1):
@@ -58,8 +76,10 @@ class Res_CBAM_block(nn.Module):
         else:
             self.shortcut = None
 
-        self.ca = ChannelAttention(out_channels)
-        self.sa = SpatialAttention()
+        self.ca_layer = ChannelAttention(out_channels)
+        self.sa_layer = SpatialAttention()
+        self.ca = None
+        self.sa = None
 
     def forward(self, x):
         residual = x
@@ -70,26 +90,51 @@ class Res_CBAM_block(nn.Module):
         out = self.relu(out)
         out = self.conv2(out)
         out = self.bn2(out)
-        out = self.ca(out) * out
-        out = self.sa(out) * out
+        if self.ca is None:
+            self.ca = self.ca_layer(out)
+        if self.sa is None:
+            self.sa = self.sa_layer(out)
+        out = self.ca * out
+        out = self.sa * out
+        out += residual
+        out = self.relu(out)
+        return out
+    
+
+class Res_SP_block(nn.Module):
+    def __init__(self, in_channels, out_channels, stride = 1):
+        super(Res_CBAM_block, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size = 3, stride = stride, padding = 1)
+        self.bn1   = nn.BatchNorm2d(out_channels)
+        self.relu  = nn.ReLU(inplace = True)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size = 3, padding = 1)
+        self.bn2   = nn.BatchNorm2d(out_channels)
+        if stride != 1 or out_channels != in_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size = 1, stride = stride),
+                nn.BatchNorm2d(out_channels))
+        else:
+            self.shortcut = None
+
+        self.sa_layer = SpatialAttention()
+        self.sa = None
+
+    def forward(self, x):
+        residual = x
+        if self.shortcut is not None:
+            residual = self.shortcut(x)
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        if self.sa is None:
+            self.sa = self.sa_layer(out)
+        out = self.sa * out
         out += residual
         out = self.relu(out)
         return out
 
-class ChannelAttention(nn.Module):
-    def __init__(self, in_planes, ratio=16):
-        super(ChannelAttention, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.max_pool = nn.AdaptiveMaxPool2d(1)
-        self.fc1   = nn.Conv2d(in_planes, in_planes // 8, 1, bias=False)
-        self.relu1 = nn.ReLU()
-        self.fc2   = nn.Conv2d(in_planes // 8, in_planes, 1, bias=False)
-        self.sigmoid = nn.Sigmoid()
-    def forward(self, x):
-        avg_out = self.fc2(self.relu1(self.fc1(self.avg_pool(x))))
-        max_out = self.fc2(self.relu1(self.fc1(self.max_pool(x))))
-        out = avg_out + max_out
-        return self.sigmoid(out)
 
 class LightWeightNetwork(nn.Module):
     def __init__(self, num_classes=1, input_channels=3, block='Res_block', num_blocks=[2,2,2,2], nb_filter=[8, 16, 32, 64, 128]):
